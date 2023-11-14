4fbec3f3cecc391a32018c369613608e60dd2c26
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-14 16:44:15 +0200

    Add Navbar & Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/core/navbar/Navbar';
+import Home from './components/core/home/Home';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <Navbar />
+
+      <Home />
     </>
   )
 }
