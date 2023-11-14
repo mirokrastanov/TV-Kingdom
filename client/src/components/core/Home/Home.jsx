7bb59a4f761bdfae383f306dea4a821422b9a8f3
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-14 16:43:54 +0200

    Create Home

---
+++ client/src/components/core/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+export default function Home() {
+
+    return (
+        <>
+            <div className="hero">
+                <h1>RULE YOUR WATCHLIST</h1>
+            </div>
+
+            <div className="container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sequi nemo perspiciatis adipisci repellendus molestiae, nesciunt ex mollitia amet cum.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus soluta unde id nulla natus nostrum laudantium minima magni! Quos, exercitationem qui! Laudantium maiores ipsa, nihil totam deleniti, quae in sit fuga possimus itaque mollitia. At labore, numquam cumque illo quae molestiae sunt non similique obcaecati beatae in expedita facere omnis!</p>
+            </div>
+        </>
+    )
+}
