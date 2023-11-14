1ef3316976d5a83ad17f8b89e08b9b1c55fa7b66
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-14 19:45:25 +0200

    Add scroll based effects & conditional class rende

--- client/src/components/core/Navbar/Navbar.jsx
+++ client/src/components/core/Navbar/Navbar.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 
 export default function Navbar() {
+    const [topScroll, setTopScroll] = useState(true);
 
-  return (
-    <header className='main-header'>
-        <div className="logo">
-            <a href="javascript:void(0)">TV Kingdom</a>
-        </div>
+    useEffect(() => {
+        // Mount
+        const scrollHandler = (e) => {
+            const scrollPos = window.scrollY;
+            if (scrollPos > 10) {
+                setTopScroll(false);
+            } else {
+                setTopScroll(true);
+            }
+        };
 
-        <input type="checkbox" className='menu-btn' id='menu-btn' />
-        <label htmlFor="menu-btn" className='menu-icon'>
-            <span className='menu-icon__line'></span>
-        </label>
+        window.addEventListener('scroll', scrollHandler);
 
-        <ul className="nav-links">
-            <li className="nav-link"><a href="javascript:void(0)">Shows</a></li>
-            <li className="nav-link"><a href="javascript:void(0)">Actors</a></li>
-            <li className="nav-link"><a href="javascript:void(0)">Schedule</a></li>
-            <li className="nav-link"><a href="javascript:void(0)">Search</a></li>
-            <li className="nav-link"><a href="javascript:void(0)">Profile</a></li> {/* Placeholder for Sign In, Sign Up or Profile Icon 
+        // Unmount
+        return () => {
+            window.removeEventListener('scroll', scrollHandler);
+        };
+    }, []);
+
+    return (
+        <header className={`main-header ${!topScroll && 'scrolled'}`}>
+            <div className="logo">
+                <a href="javascript:void(0)">TV Kingdom</a>
+            </div>
+
+            <input type="checkbox" className='menu-btn' id='menu-btn' />
+            <label htmlFor="menu-btn" className='menu-icon'>
+                <span className='menu-icon__line'></span>
+            </label>
+
+            <ul className="nav-links">
+                <li className="nav-link"><a href="javascript:void(0)">Shows</a></li>
+                <li className="nav-link"><a href="javascript:void(0)">Actors</a></li>
+                <li className="nav-link"><a href="javascript:void(0)">Schedule</a></li>
+                <li className="nav-link"><a href="javascript:void(0)">Search</a></li>
+                <li className="nav-link"><a href="javascript:void(0)">User</a></li> {/* Placeholder for Sign In, Sign Up or Profile Icon 
             - figure out where it goes on mobile res */}
-        </ul>
-    </header>
-  )
+            </ul>
+        </header>
+        // the profile can be here as well - TODO later on!
+    )
 }
