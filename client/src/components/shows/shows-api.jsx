41e377d7a8d6d9097683427f59c85defd7fec053
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-14 16:01:08 +0200

    Init API files

---
+++ client/src/components/shows/shows-api.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function showsAPI() {
+    
+  return (
+    <div>showsAPI</div>
+  )
+}
---
+++ client/src/components/user/user-api.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function userAPI() {
+  
+    return (
+    <div>userAPI</div>
+  )
+}
