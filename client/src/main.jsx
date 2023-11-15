0f0c323ab014774c15282c6bca718cfb105e2636
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-16 00:31:28 +0200

    Provide the theme context to the app's top level

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider> {/* DARK MODE CONTEXT */}
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
