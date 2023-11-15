bdf5ed02d78833af02e98c3b11aa194a35da39b0
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-15 21:41:37 +0200

    Update App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.module.css'
-import Navbar from './components/core/navbar/Navbar';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import Home from './components/core/home/Home';
+import Navigation from './components/core/navigation/Navigation';
+import Footer from './components/core/footer/Footer';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Navbar />
+    <div className='app-ctr'>
+      <Navigation />
 
       <Home />
-    </>
+
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
