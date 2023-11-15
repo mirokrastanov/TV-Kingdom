094b5c011b295a2a09cd9069c3773dd7ee946aca
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-15 21:39:17 +0200

    Replace the master fragment with a div

--- client/src/components/core/Home/Home.jsx
+++ client/src/components/core/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Home.module.css';
+import './Home.css';
 
 export default function Home() {
 
     return (
-        <>
+        <div className='home-ctr'>
             <div className="hero">
                 <h1>RULE YOUR WATCHLIST</h1>
             </div>
@@ -14,6 +14,6 @@ export default function Home() {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus soluta unde id nulla natus nostrum laudantium minima magni! Quos, exercitationem qui! Laudantium maiores ipsa, nihil totam deleniti, quae in sit fuga possimus itaque mollitia. At labore, numquam cumque illo quae molestiae sunt non similique obcaecati beatae in expedita facere omnis!</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi suscipit excepturi eius recusandae laboriosam aperiam nobis sint? Voluptatem amet cupiditate corrupti! Eius quasi quae eveniet laborum soluta delectus, perspiciatis similique totam unde, molestiae nesciunt harum possimus ad eaque deleniti iste natus consequuntur odio recusandae? Dolores, tenetur voluptatibus praesentium impedit unde voluptates, consectetur corporis nulla nihil velit ratione debitis eligendi ad animi est ex officia maxime enim quaerat iusto deserunt perferendis dolorem repellat. Odit nostrum quasi qui excepturi sed. A doloremque error sequi et optio harum magni natus qui necessitatibus minima, reprehenderit quae veritatis quasi, voluptatem non illum nihil at temporibus minus obcaecati ipsam, rerum perferendis. Blanditiis nisi ipsam, placeat fugiat incidunt sequi dolorem voluptas nihil. Et, eum. Labore, rerum. Ipsa suscipit officia praesentium alias corrupti, esse assumenda debitis veniam sequi maxime numquam, illum natus temporibus! Aspernatur officia soluta nam explicabo error consectetur ab facilis dolores omnis, amet dolore et culpa, tempora saepe possimus perspiciatis ratione quam. Nulla saepe rerum totam magnam in quia, alias labore, vitae ad dolorem facere sapiente eligendi tempore dolor iste error. Inventore ad quis repudiandae neque ut laudantium facilis error ab voluptatibus. Consequuntur blanditiis ipsam itaque minus sit corporis sed illum soluta sequi, qui doloremque.</p>
             </div>
-        </>
+        </div>
     )
 }
