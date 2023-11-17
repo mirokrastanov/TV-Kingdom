e9e064b1e2d1db50ac568e3cec8aa88ad2b3dd6c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-17 14:26:13 +0200

    Add react router and remove strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider> {/* DARK MODE CONTEXT */}
+  <ThemeProvider> {/* DARK MODE CONTEXT */}
+    <BrowserRouter>
       <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </ThemeProvider>
 )
