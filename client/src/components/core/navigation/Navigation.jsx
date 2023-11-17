7392453d35dc5488a7d2cdd15d3a444e1e55f8f5
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-17 15:53:56 +0200

    Update navigation

--- client/src/components/core/navigation/Navigation.jsx
+++ client/src/components/core/navigation/Navigation.jsx
@@ -41,23 +41,36 @@ export default function Navigation() {
                 <li className="nav-link a-left"><a href="javascript:void(0)">Shows</a></li>
                 <li className="nav-link a-left"><a href="javascript:void(0)">Actors</a></li>
                 <li className="nav-link a-left"><a href="javascript:void(0)">Schedule</a></li>
-                <li className="nav-link a-left" title='Search'>
-                    <a href="javascript:void(0)"><span class="material-symbols-outlined">search</span></a>
-                </li>
-                <li className="nav-link a-left" title='Log In'>
-                    <a href="javascript:void(0)"><span class="material-symbols-outlined">login</span></a>
-                </li>
-                <li className="nav-link a-left" title='Sign Up'>
-                    <a href="javascript:void(0)"><span class="material-symbols-outlined">person_add</span></a>
-                </li>
-                <li className="nav-link a-left" title='Log Out'>
-                    <a href="javascript:void(0)"><span class="material-symbols-outlined">logout</span></a>
-                </li>
-                <li className="nav-link a-left" title='Profile Modal'> {/* Change title when the modal is implemented */}
-                    <a href="javascript:void(0)"><span class="material-symbols-outlined">account_circle</span></a>
-                </li>
+                <li className="nav-link a-left"><a href="javascript:void(0)">Log In</a></li>
+                <li className="nav-link a-left"><a href="javascript:void(0)">Sign Up</a></li>
+
+                {/* Future protected routes */}
+                {/* <li className="nav-link a-left"><a href="javascript:void(0)">Profile</a></li> */}
             </ul>
         </header>
         // the profile can be here as well - TODO later on!
     )
 }
+
+
+
+/*
+
+<li className="nav-link a-left" title='Search'>
+    <a href="javascript:void(0)"><span className="material-symbols-outlined">search</span></a>
+</li>
+<li className="nav-link a-left" title='Log In'>
+    <a href="javascript:void(0)"><span className="material-symbols-outlined">login</span></a>
+</li>
+<li className="nav-link a-left" title='Sign Up'>
+    <a href="javascript:void(0)"><span className="material-symbols-outlined">person_add</span></a>
+</li>
+<li className="nav-link a-left" title='Log Out'>
+    <a href="javascript:void(0)"><span className="material-symbols-outlined">logout</span></a>
+</li>
+<li className="nav-link a-left" title='Profile Modal'> 
+// Change title when the modal is implemented
+<a href="javascript:void(0)"><span className="material-symbols-outlined">account_circle</span></a>
+</li>
+
+*/
