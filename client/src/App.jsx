e84d6104205b8399eea5d28550be6f6880683b08
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-17 14:29:54 +0200

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import { useTheme } from './contexts/ThemeContext';
+import './App.css';
 import Home from './components/core/home/Home';
 import Navigation from './components/core/navigation/Navigation';
 import Footer from './components/core/footer/Footer';
 
 function App() {
-  const darkTheme = useTheme();
+    const darkTheme = useTheme();
 
-  return (
-    <div className={`app-ctr${darkTheme ? ' dark-mode' : ''}`}>
-      <Navigation />
+    return (
+        <div className={`app-ctr${darkTheme ? ' dark-mode' : ''}`}>
+            <Navigation />
 
-      <Home /> {/* TO ADD ROUTES */}
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
 
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
