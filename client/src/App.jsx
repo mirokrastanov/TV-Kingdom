f41e789d0f4ad44d3d3efc5a8f0925b4f002394d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-19 23:52:11 +0200

    Add additional routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useTheme } from './contexts/ThemeContext';
 import './App.css';
 import Home from './components/home/Home';
 import Navigation from './components/navigation/Navigation';
-import Footer from './components/footer/Footer';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
     const darkTheme = useTheme();
@@ -12,11 +12,17 @@ function App() {
         <div className={`app-ctr${darkTheme ? ' dark-mode' : ''}`}>
             <Navigation />
 
-            <Routes>
-                <Route path='/' element={<Home />} />
-            </Routes>
+            <div className="app-content">
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/home' element={<Navigate to='/' />} />
+                    <Route path='/index' element={<Navigate to='/' />} />
+                    <Route path='/index.html' element={<Navigate to='/' />} />
 
-            <Footer />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </div>
+
         </div>
     )
 }
