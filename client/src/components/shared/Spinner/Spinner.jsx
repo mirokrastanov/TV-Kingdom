208dd2bf033c2bcc39799b0daa825cd5012620a9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 19:16:14 +0200

    Create Spinner component

---
+++ client/src/components/shared/Spinner/Spinner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Spinner.css';
+
+export default function Spinner() {
+
+    return (
+        <div class="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+};
