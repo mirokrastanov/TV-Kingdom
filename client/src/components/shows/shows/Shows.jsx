14a6927ebf7fe0b67c80375f3c4a4283f31eabb1
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 15:22:53 +0200

    Create Shows component

---
+++ client/src/components/shows/shows/Shows.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Shows.css';
+
+export default function Shows() {
+
+    return (
+        <div className="shows-ctr">
+            <h1>Shows</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+}
