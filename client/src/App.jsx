e59a61e34f953b7484dbc209477883a65529b147
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 19:05:00 +0200

    Add multiple new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,11 @@ import './App.css';
 import Home from './components/home/Home';
 import Navigation from './components/navigation/Navigation';
 import NotFound from './components/notFound/NotFound';
-import Shows from './components/shows/shows/Shows';
+import Shows from './components/shows/Shows';
+import UserSignIn from './components/userSignIn/UserSignIn';
+import Actors from './components/actors/Actors';
+import Schedule from './components/schedule/Schedule';
+import Search from './components/search/Search';
 
 function App() {
     const darkTheme = useTheme();
@@ -16,9 +20,19 @@ function App() {
             <div className="app-content">
                 <Routes>
                     <Route exact path='/' element={<Home />} />
-                    <Route path='/home' element={<Navigate to='/' />} />
-                    <Route path='/index' element={<Navigate to='/' />} />
-                    <Route path='/shows' element={<Shows />} />
+                    <Route path='home' element={<Navigate to='/' />} />
+                    <Route path='index' element={<Navigate to='/' />} />
+                    
+                    <Route path='shows' element={<Shows />} />
+                    <Route path='actors' element={<Actors />} />
+                    <Route path='schedule' element={<Schedule />} />
+                    <Route path='search' element={<Search />} />
+
+                    <Route path='user'>
+                        <Route path='sign-in' element={<UserSignIn />} />
+                        <Route path='sign-up' element={<Shows />} />
+                        <Route path='profile' element={<Shows />} />
+                    </Route>
 
                     <Route path='*' element={<NotFound />} />
                 </Routes>
