f5be908b1b6631f67ad3e50a9e35201065b6cc9a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 19:03:16 +0200

    Create User Sign In component

---
+++ client/src/components/userSignIn/UserSignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserSignIn.css';
+
+export default function UserSignIn() {
+
+    return (
+        <div className="sign-in-ctr">
+            <h1>Sign In FORM</h1>
+            <h2>Also add a Sign Up link somewhere</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+};
