5de8deb0d56a826ac5725afcaf81797b55a03ca4
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 15:23:12 +0200

    Create show card component

---
+++ client/src/components/shows/showCard/ShowCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ShowCard.css';
+
+export default function ShowCard() {
+
+    return (
+        <div className="show-card-ctr">
+            <h1>Show Card</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+}
