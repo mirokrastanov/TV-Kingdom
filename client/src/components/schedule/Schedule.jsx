14ba2b74e2657d7d0f61165a4a75a0eb1421d50b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 19:02:22 +0200

    Create Schedule component

---
+++ client/src/components/schedule/Schedule.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Schedule.css';
+
+export default function Schedule() {
+
+    return (
+        <div className="schedule-ctr">
+            <h1>Schedule</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+}
