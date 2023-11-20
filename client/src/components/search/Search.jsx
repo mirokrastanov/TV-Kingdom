0876466e27ed3137804c0ef2d21dfbacbff20d67
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 19:02:51 +0200

    Create Search component

---
+++ client/src/components/search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Search.css';
+
+export default function Search() {
+
+    return (
+        <div className="search-page-ctr">
+            <h1>Search</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+};
