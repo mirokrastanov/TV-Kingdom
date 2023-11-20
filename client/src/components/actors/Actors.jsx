d1632a25b3d7acc461b61ef9996a4f8b2d6eb72f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-20 19:02:05 +0200

    Create Actors component

---
+++ client/src/components/actors/Actors.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Actors.css';
+
+export default function Actors() {
+
+    return (
+        <div className="actors-ctr">
+            <h1>Actors</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+};
