f79e23e77c2046dca1eb4ee312c7480dafbf7334
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-21 18:05:58 +0200

    Update service to accept a page argument

--- client/src/services/showsService.js
+++ client/src/services/showsService.js
@@ -2,9 +2,10 @@ const baseURL = 'https://api.tvmaze.com';
 
 
 export const shows = {
-    all: async function () {
-        const res = await fetch(`${baseURL}/shows`);
+    all: async function (page = 0) {
+        const res = await fetch(`${baseURL}/shows?page=${page}`);
         const data = await res.json();
         return data;
     },
+
 };
