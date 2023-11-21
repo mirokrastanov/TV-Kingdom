55ece28f7342233b18a655d48c16bc6a8adcfe36
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-21 18:06:30 +0200

    Add fetch buttons

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
+import { shows } from '../../services/showsService';
+
+const INITIAL_STATE = {
+    showsData: {},
+    showsPage: 0,
+};
 
 export default function Home() {
+    const [pageValues, setPageValues] = useState(INITIAL_STATE);
+
+    const nextShowsPage = () => {
+        setPageValues({ ...pageValues, showsPage: pageValues.showsPage + 1 });
+    };
+
+    const prevShowsPage = () => {
+        if (pageValues.showsPage - 1 < 0) return;
+        setPageValues({ ...pageValues, showsPage: pageValues.showsPage - 1 });
+    };
+
+    const fetchShowsPage = async () => {
+        const data = await shows.all(pageValues.showsPage);
+        setPageValues({ ...pageValues, showsData: data })
+        console.log(data);
+    };
 
     return (
         <div className='home-ctr'>
@@ -10,6 +32,11 @@ export default function Home() {
             </div>
 
             <div className="container">
+                <div className="btn-cage">
+                    <button className='btn' onClick={prevShowsPage}>Prev page: {pageValues.showsPage - 1 >= 0 ? pageValues.showsPage - 1 : 'X'}</button>
+                    <button className='btn' onClick={fetchShowsPage}>Fetch page: {pageValues.showsPage}</button>
+                    <button className='btn' onClick={nextShowsPage}>Next page: {pageValues.showsPage + 1}</button>
+                </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sequi nemo perspiciatis adipisci repellendus molestiae, nesciunt ex mollitia amet cum.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus soluta unde id nulla natus nostrum laudantium minima magni! Quos, exercitationem qui! Laudantium maiores ipsa, nihil totam deleniti, quae in sit fuga possimus itaque mollitia. At labore, numquam cumque illo quae molestiae sunt non similique obcaecati beatae in expedita facere omnis!</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi suscipit excepturi eius recusandae laboriosam aperiam nobis sint? Voluptatem amet cupiditate corrupti! Eius quasi quae eveniet laborum soluta delectus, perspiciatis similique totam unde, molestiae nesciunt harum possimus ad eaque deleniti iste natus consequuntur odio recusandae? Dolores, tenetur voluptatibus praesentium impedit unde voluptates, consectetur corporis nulla nihil velit ratione debitis eligendi ad animi est ex officia maxime enim quaerat iusto deserunt perferendis dolorem repellat. Odit nostrum quasi qui excepturi sed. A doloremque error sequi et optio harum magni natus qui necessitatibus minima, reprehenderit quae veritatis quasi, voluptatem non illum nihil at temporibus minus obcaecati ipsam, rerum perferendis. Blanditiis nisi ipsam, placeat fugiat incidunt sequi dolorem voluptas nihil. Et, eum. Labore, rerum. Ipsa suscipit officia praesentium alias corrupti, esse assumenda debitis veniam sequi maxime numquam, illum natus temporibus! Aspernatur officia soluta nam explicabo error consectetur ab facilis dolores omnis, amet dolore et culpa, tempora saepe possimus perspiciatis ratione quam. Nulla saepe rerum totam magnam in quia, alias labore, vitae ad dolorem facere sapiente eligendi tempore dolor iste error. Inventore ad quis repudiandae neque ut laudantium facilis error ab voluptatibus. Consequuntur blanditiis ipsam itaque minus sit corporis sed illum soluta sequi, qui doloremque.</p>
