f57582cc4012169dcc4bf8d726b5235316b7f70a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-21 19:25:20 +0200

    Implement a custom get function for shows service

--- client/src/services/showsService.js
+++ client/src/services/showsService.js
@@ -1,15 +1,28 @@
 const baseURL = 'https://api.tvmaze.com';
 
+const get = async (address) => {
+    try {
+        const resRaw = await fetch(address, { method: 'GET' });
+        let resData = [];
+        if (resRaw.ok == false) throw resRaw;
+        if (resRaw.status != 204) resData = await resRaw.json();
+        return resData;
+    } catch (err) {
+        if (err.message) console.log(err.message);
+        else if (err.stateText) console.log(err.stateText);
+        else console.log(err);
+        // re-throw if needed for global error handler or for page specific one
+    }
+};
+
 
 export const shows = {
     all: async function (page = 0) {
-        const res = await fetch(`${baseURL}/shows?page=${page}`);
-        const data = await res.json();
+        const data = await get(`${baseURL}/shows?page=${page}`);
         return data;
     },
     one: async function (id) {
-        const res = await fetch(`${baseURL}/shows/${id}?embed=cast`);
-        const data = await res.json();
+        const data = await get(`${baseURL}/shows/${id}?embed=cast`);
         return data;
     },
 
