5f9fb91ee3dbceda2f308b0e1a2014b70353b75e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-21 15:28:51 +0200

    Add first fetch function

--- client/src/services/showsService.js
+++ client/src/services/showsService.js
@@ -1,4 +1,10 @@
 const baseURL = 'https://api.tvmaze.com';
 
 
-
+export const shows = {
+    all: async function () {
+        const res = await fetch(`${baseURL}/shows`);
+        const data = await res.json();
+        return data;
+    },
+};
