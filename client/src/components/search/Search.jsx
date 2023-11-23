b21e19c1f53e23bc06e28b9009a6d87bf53eedd8
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-23 22:28:44 +0200

    Update search page - main form ctr + styles

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -8,10 +8,10 @@ export default function Search() {
             <div id="form-ctr">
                 <h1>Search</h1>
                 <form>
-                    <input type="text" placeholder='Type Keywords...' />
-                    <span class="s-icon material-symbols-outlined">search</span>
+                    <input type="text" placeholder='Type Keywords...' name='search' />
+                    <span className="s-icon material-symbols-outlined">search</span>
                 </form>
-                <h3>Search among thousands of shows, actors and crew.</h3>
+                <h4>Navigate through an extensive database featuring a diverse array of shows, actors, and crew members.</h4>
             </div>
             <div id="results-ctr">
                 {/* New component here. Would receive props from search res */}
