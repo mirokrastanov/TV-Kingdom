7b9336753f00720aed69efca841d5f51b2f06a66
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-23 20:39:35 +0200

    Update search page

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -5,8 +5,17 @@ export default function Search() {
 
     return (
         <div className="search-page-ctr">
-            <h1>Search</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+            <div id="form-ctr">
+                <h1>Search</h1>
+                <form>
+                    <input type="text" placeholder='Type Keywords...' />
+                    <span class="s-icon material-symbols-outlined">search</span>
+                </form>
+                <h3>Search among thousands of shows, actors and crew.</h3>
+            </div>
+            <div id="results-ctr">
+                {/* New component here. Would receive props from search res */}
+            </div>
         </div>
     )
 };
