0b99289f88c9cb693a8f9e07a84fd4c81b90b28c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-24 19:59:18 +0200

    Initial dynamic rendering logic + template update

--- client/src/components/shared/card/Card.jsx
+++ client/src/components/shared/card/Card.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Card.css';
 
-export default function Card(p) {
+export default function Card(props) {
+    // id, name, language, genres[], premiered, ended, rating.average, network.name, image.medium. summmary
+
+    let p = { // Mockup data for pre-rendering, testing & to replace missing assets at start
+        id: 82, name: 'Game of Thrones', genres: ['Drama', 'Adventure', 'Fantasy'], premiered: '2011-04-17', ended: '2019-05-19',
+        rating: { average: 8.9 }, network: { name: 'HBO' }, image: { medium: 'https://posterspy.com/wp-content/uploads/2020/10/got.jpg' },
+        summary: '<p>Based on the bestselling book series <i>A Song of Ice and Fire</i> by George R.R. Martin, this sprawling new HBO drama is set in a world where summers span decades and winters can last a lifetime. From the scheming south and the savage eastern lands, to the frozen north and ancient Wall that protects the realm from the mysterious darkness beyond, the powerful families of the Seven Kingdoms are locked in a battle for the Iron Throne. This is a story of duplicity and treachery, nobility and honor, conquest and triumph. In the <b>Game of Thrones</b>, you either win or you die.</p>'
+    };
+
+    const extractYear = (date) => (date.split('-'))[0];
+    const plotRating = (rating) => {
+        let output = [];
+        for (let i = 1; i <= 10; i++) {
+            if (i <= rating) output.push(1);
+            else if (i - 1 < rating && i > rating) output.push(0.5);
+            else if (i > rating ) output.push(0);
+        }
+        return output;
+    };
+
+    useEffect(() => {
+        if (props.id) {
+            p = props;
+        }
+
+        return () => { };
+    }, [props]);
+
 
     return (
         <div className="card">
             <div className="poster">
-                <img src="https://posterspy.com/wp-content/uploads/2020/10/got.jpg" alt="card-poster" />
-                /</div>
+                <img src={p.image.medium} alt="card-poster" />
+            </div>
             <div className="details">
-                <div className='title-ctr'><h2>The Chronicles of Narnia: The Lion, the Witch and the Wardrobe</h2></div>
-                <span>2017 - 2023</span>
+                <div className='title-ctr'><h2>{p.name}</h2></div>
+                <span>{extractYear(p.premiered)} - {extractYear(p.ended)}</span>
 
                 <div className="rating">
-                    <span className="material-symbols-outlined filled-symbol">star</span>
+                    {plotRating(p.rating.average).map((x, i) => {
+                        if (x == 1) return(<span key={`rating-${i}-${p.id}`} className="material-symbols-outlined filled-symbol">star</span>);
+                        else if (x > 0 & x < 1) return (<span key={`rating-${i}-${p.id}`} className="material-symbols-outlined">star_half</span>); 
+                        else return (<span key={`rating-${i}-${p.id}`} className="material-symbols-outlined">star</span>);
+                    })}
+
+                    {/* <span className="material-symbols-outlined filled-symbol">star</span>
                     <span className="material-symbols-outlined filled-symbol">star</span>
                     <span className="material-symbols-outlined filled-symbol">star</span>
                     <span className="material-symbols-outlined filled-symbol">star</span>
@@ -22,7 +55,7 @@ export default function Card(p) {
                     <span className="material-symbols-outlined filled-symbol">star</span>
                     <span className="material-symbols-outlined filled-symbol">star</span>
                     <span className="material-symbols-outlined">star_half</span>
-                    <span className="material-symbols-outlined">star</span>
+                    <span className="material-symbols-outlined">star</span> */}
                     <span className="rating-number">8.6/10</span>
                 </div>
                 <div className="tags">
