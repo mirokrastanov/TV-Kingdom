7b64b7e81da21989e8c9a73e661fc2480046fe19
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-24 17:02:40 +0200

    Card related updates

--- client/src/components/shows/Shows.jsx
+++ client/src/components/shows/Shows.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Shows.css';
+import Card from '../shared/card/card';
 
 export default function Shows() {
 
     return (
         <div className="shows-ctr">
             <h1>Shows</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+            <div className="cards-cage">
+                <Card />
+            </div>
         </div>
     )
 };
