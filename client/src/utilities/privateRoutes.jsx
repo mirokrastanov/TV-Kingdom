b8d9994721be1fcf488b6c597f9c8a0791880a4d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-30 22:29:09 +0200

    Auth now state based and updated dynamically

--- client/src/utilities/privateRoutes.jsx
+++ client/src/utilities/privateRoutes.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext.jsx';
 
 export default function PrivateRoutes() {
-    const user = false;
+    const { user } = useAuth(); // import wherever u want to check if there is a user logged in
     return user
         ? (<Outlet />)
         : (<Navigate to='/user/sign-up' />)
