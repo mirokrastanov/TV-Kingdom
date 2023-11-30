8fb8ae5707bf511cf9345f9202c8acdde0814e9c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-30 17:23:58 +0200

    Wrap the app inside Auth Context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './contexts/authContext.jsx';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
-import App from './App.jsx';
 import { SearchProvider } from './contexts/SearchContext.jsx';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider> {/* DARK MODE CONTEXT */}
-    <SearchProvider> {/* SEARCH VALUE CONTEXT */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SearchProvider>
-  </ThemeProvider>
+  <AuthProvider> {/* AUTH CONTEXT */}
+    <ThemeProvider> {/* DARK MODE CONTEXT */}
+      <SearchProvider> {/* SEARCH VALUE CONTEXT */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SearchProvider>
+    </ThemeProvider>
+  </AuthProvider>
 )
