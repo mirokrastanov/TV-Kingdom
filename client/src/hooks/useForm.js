85aa8c5fd9eb9c76d6a52c2b13eaee85232f5944
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-30 22:27:55 +0200

    Add extra functions to hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -9,7 +9,6 @@ export const useForm = (submitHandler, initialValues) => {
             ...state,
             [e.target.name]: e.target.value
         }));
-
     };
 
     const onSubmit = (e) => {
@@ -18,9 +17,26 @@ export const useForm = (submitHandler, initialValues) => {
         submitHandler(values);
     };
 
+    const onFocus = (e) => {
+        setValues(state => ({
+            ...state,
+            [`${e.target.name}Focus`]: true,
+        }));
+    };
+
+    const onBlur = (e) => {
+        setValues(state => ({
+            ...state,
+            [`${e.target.name}Focus`]: false,
+        }));
+    };
+
     return {
         values,
+        setValues,
         onChange,
         onSubmit,
+        onFocus,
+        onBlur,
     }
 };
