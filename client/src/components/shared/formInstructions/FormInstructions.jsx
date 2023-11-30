8492c5d837b6cadee1dceb5dc0bfa98fef1e2671
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-30 23:51:27 +0200

    Create Form Instructions component

---
+++ client/src/components/shared/formInstructions/FormInstructions.jsx
@@ -0,0 +1,31 @@
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect } from 'react';
+
+const messages = {
+    emailnote: [(<> Enter a valid <b>Email</b></>), (<> Eg: <b>john@example.com</b></>)],
+    pwdnote: [
+        (<> 8 to 24 characters</>),
+        (<> Required: <b>Uppercase</b>, <b>Lowercase</b> & <b>Number</b></>),
+        (<> Optional: Allowed special characters:</>),
+        (<><b> ! . _ @ # $ %</b></>)
+    ],
+
+};
+
+export function FormInstructions({ id, className }) {
+
+    useEffect(() => {
+        //   console.log(text, validProp, prop);
+    }, [])
+
+    return (
+        <div id={id} className={className}>
+            {messages[id].map((x, i) => (
+                <p key={`${id}-${i}`}>
+                    {!(id == 'pwdnote' && i == 3) && (<FontAwesomeIcon icon={faInfoCircle} />)}{x}
+                </p>
+            ))}
+        </div>
+    );
+};
