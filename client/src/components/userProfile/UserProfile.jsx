223be356d5bb460299edc29101d5e45c047ce462
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-01 13:58:36 +0200

    Create user profile

---
+++ client/src/components/userProfile/UserProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './UserProfile.css';
+
+function UserProfile() {
+
+    return (
+        <div className="user-profile-ctr">
+            <h1>User Profile</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+}
+
+export default UserProfile;
