ca45af39515c2efe0ea216c2275da3bae49f25e4
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-04 17:54:21 +0200

    Add details to season cards

--- client/src/components/showSeasons/showSeasons.jsx
+++ client/src/components/showSeasons/showSeasons.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './ShowSeasons.css';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { shows } from '../../services/showService';
 import PageLoader from '../shared/pageLoader/PageLoader';
+import SummaryComplete from '../shared/summary/SummaryComplete';
+
 
 export default function ShowSeasons() {
     const { showId } = useParams();
     const [p, setP] = useState({});
     const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
     useEffect(() => {
         console.log(showId);
@@ -25,15 +28,33 @@ export default function ShowSeasons() {
         return () => { };
     }, []);
 
+    function routeTo(e) {
+        const url = '/seasons/' + e.currentTarget.dataset.id;
+        navigate(url);
+    };
+
     return (<>
         {loading
             ? (<PageLoader />)
             : (<div className="show-extra-ctr">
-                <h1>Show Name</h1>
-                <h2>Seasons</h2>
-                <div className="show-extra-data">
-
-                </div>
+                {p.map(x => (
+                    <div key={`${x.id}-show-seasons`} data-id={x.id} className="show-extra-data" onClick={routeTo}>
+                        <div className="s-n">{x.number}</div>
+                        <article>
+                            {x.image
+                                ? (<img src={x.image.original} alt="season-img" />)
+                                : (<img style={{ background: 'var(--color-accent-2)' }} src='/src/assets/replace-img.jpg' alt="season-img" />)}
+                        </article>
+                        <article>
+                            {x.summary
+                                ? (<SummaryComplete summary={x.summary} />)
+                                : (<p>We don't have a summary for Season {x.number} yet.</p>)}
+                            <p>Episodes: <b>{x.episodeOrder}</b></p>
+                            <p>Premiere: <b>{x.premiereDate}</b></p>
+                            <p>Ended: <b>{x.endDate ? x.endDate : '-'}</b></p>
+                        </article>
+                    </div>
+                ))}
             </div>)
         }
     </>);
