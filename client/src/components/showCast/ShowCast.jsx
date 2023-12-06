1c7e31a5bf096d005787370b6615d749ae9de96b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-06 15:23:57 +0200

    Create component

---
+++ client/src/components/showCast/ShowCast.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ShowCast.css';
+
+
+export default function ShowCast() {
+
+    return (
+        <div className="show-cast-ctr">
+            <h1>Show Cast</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore repudiandae eius dolorum enim. Nobis quo vitae placeat neque itaque. Illum?</p>
+        </div>
+    )
+}
