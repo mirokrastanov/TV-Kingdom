680e2b4179f183f0041862ffb8719aaa70a28cf2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-06 23:46:57 +0200

    Dynamic search page design with dual res sets

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -4,11 +4,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useSearch, useSearchUpdate } from '../../contexts/SearchContext';
 import { useAuth } from '../../contexts/AuthContext.jsx';
 import { useEffect } from 'react';
+import { search } from '../../services/showService.js';
+import PageLoader from '../shared/pageLoader/PageLoader.jsx';
+import ShowCard from '../showCard/ShowCard.jsx';
+import ActorCard from '../actorCard/ActorCard.jsx';
 
 
 export default function Search() {
-    const [results, setResults] = useState([1, 2, 3]);
-
+    const [showResults, setShowResults] = useState([]);
+    const [personResults, setPersonResults] = useState([]);
+    const [resType, setResType] = useState('shows');
+    const [loading, setLoading] = useState(true);
     const { user } = useAuth();
     const searchValue = useSearch();
     const updateSearchValue = useSearchUpdate();
@@ -21,22 +27,52 @@ export default function Search() {
     useEffect(() => {
         // console.log(searchValue);
         if (searchValue != '') loadResults();
+        setLoading(false);
 
-        return () => { updateSearchValue('') }
+        // return () => { updateSearchValue('') }
     }, []);
 
     function searchOnChangeHandler(e) {
         updateSearchValue(e.target.value);
     };
 
-    function searchOnSubmitHandler(e) {
+    async function searchOnSubmitHandler(e) {
         e.preventDefault();
+        if (searchValue == '') return;
 
-
+        await loadResults();
     };
 
-    async function loadResults() {
+    function switchSelected() {
+        if (resType == 'shows') setResType('actors');
+        else setResType('shows');
+    }
 
+    async function loadResults() {
+        setLoading(true);
+
+        search.shows(searchValue)
+            .then(sData => {
+                console.log(sData);
+                setShowResults(sData);
+                setLoading(false);
+
+                search.actors(searchValue)
+                    .then(pData => {
+                        // console.log(pData);
+                        setPersonResults(pData);
+
+                        setLoading(false);
+                    })
+                    .catch(err => {
+                        // console.log(err.message);
+                        setLoading(false);
+                    })
+            })
+            .catch(err => {
+                // console.log(err.message);
+                setLoading(false);
+            })
     };
 
     return (
@@ -57,12 +93,24 @@ export default function Search() {
                 </form>
                 <h4>Navigate through an extensive database featuring a diverse array of shows, actors, and crew members.</h4>
             </div>
-            <div id="results-ctr">
-                {results.map(x => (
-                    <div key={x}>x</div>
-                    // create actorCard first - keep size the same az show card - just switch content inside
-                    // determine whether the result is a show or a person and render showCard or actorCard for it
-                ))}
+            <div id="controls-res" onClick={switchSelected}>
+                <div className={resType == 'shows' ? 'selected-res' : ''}>Shows</div>
+                <div className={resType == 'actors' ? 'selected-res' : ''}>People</div>
+            </div>
+            <div id="showResults-ctr">
+                {searchValue != '' && (showResults.length > 0 || personResults.length) > 0 && (loading
+                    ? (<PageLoader />)
+                    : (<>
+                        <div id="s-r-1" className={`${resType == 'actors' ? 'hidden-res-ctr' : ''} cards-cage`}>
+                            {showResults.map(s => (<ShowCard key={`${s.show.id}-shows`} {...s.show} />))}
+                            {showResults.length == 0 && (<h2>No results</h2>)}
+                        </div>
+                        <div id="p-r-2" className={`${resType == 'shows' ? 'hidden-res-ctr' : ''} cards-cage`}>
+                            {personResults.map(p => (<ActorCard key={`${p.person.id}-person`} {...p.person} />))}
+                            {personResults.length == 0 && (<h2>No results</h2>)}
+                        </div>
+                    </>)
+                )}
             </div>
         </div>
     )
