9f4063fbd77981357f8a2a95b2e578a3bad4bbde
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-07 13:37:14 +0200

    Add database and collection

--- client/src/config/appwriteConfig.js
+++ client/src/config/appwriteConfig.js
@@ -1,7 +1,9 @@
-import { Client, Account } from 'appwrite';
+import { Client, Account, Databases } from 'appwrite';
 
 export const API_ENDPOINT = 'https://cloud.appwrite.io/v1';
 export const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_APPWRITE_COLLECTION_ID_MESSAGES;
 
 const client = new Client();
 client
@@ -9,5 +11,6 @@ client
     .setProject(PROJECT_ID);
 
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export default client;
