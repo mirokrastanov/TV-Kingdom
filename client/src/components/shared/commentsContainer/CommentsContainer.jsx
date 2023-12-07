8de2930eb59ad207df6a33fc3e6042147ed679b0
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-07 16:19:02 +0200

    Complete Create, Delete & Permissions

--- client/src/components/shared/commentsContainer/CommentsContainer.jsx
+++ client/src/components/shared/commentsContainer/CommentsContainer.jsx
@@ -4,48 +4,44 @@ import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../../..
 import { ID, Query, Permission, Role } from 'appwrite';
 import { useAuth } from '../../../contexts/AuthContext';
 import { Trash2 } from 'react-feather';
+import { useParams } from 'react-router-dom';
 
 
 export default function CommentsContainer() {
     const [messageBody, setMessageBody] = useState('');
     const [messages, setMessages] = useState([]);
     const { user } = useAuth();
-
+    const { showId, actorId, episodeId } = useParams();
+    const subscribeQuery = `databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGES}.documents`;
 
     useEffect(() => {
         getMessages();
+        // console.log(showId, actorId, episodeId);
+        // console.log(showId ? [showId, 'show'] : (actorId ? [actorId, 'actor'] : [episodeId, 'ep']));
 
+        const unsubscribe = client.subscribe(subscribeQuery, response => {
+            // console.log('REAL TIME: ', response);
+            if (response.events.includes("databases.*.collections.*.documents.*.create")) {
+                // console.log('A MESSAGE WAS CREATED');
+                setMessages(prevState => [response.payload, ...prevState]);
+            }
+            if (response.events.includes("databases.*.collections.*.documents.*.update")) {
+                // add edit logic later
+            }
+            if (response.events.includes("databases.*.collections.*.documents.*.delete")) {
+                // console.log('A MESSAGE WAS DELETED!!!');
+                setMessages(prevState => prevState.filter(message => message.$id !== response.payload.$id));
+            }
+        });
+        // console.log('unsubscribe:', unsubscribe);
+
+        return () => { unsubscribe() };
     }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('MESSAGE:', messageBody);
-
-        const payload = {
-            body: messageBody
-        };
-
-        const response = await databases.createDocument(
-            DATABASE_ID,
-            COLLECTION_ID_MESSAGES,
-            ID.unique(),
-            payload,
-        );
-
-        console.log('RESPONSE:', response);
-
-        setMessages(prevState => [response, ...prevState]);
-
-        setMessageBody('');
-
-    }
-
-    const deleteMessage = async (id) => {
-        await databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, id);
-        setMessages(prevState => prevState.filter(message => message.$id !== id));
-    }
-
     const getMessages = async () => {
+        let asset = showId ? [showId, 'show'] : (actorId ? [actorId, 'actor'] : [episodeId, 'ep']);
+        let assetId = `${asset[1]}--${asset[0]}`;
+
         const response = await databases.listDocuments(
             DATABASE_ID,
             COLLECTION_ID_MESSAGES,
@@ -54,10 +50,48 @@ export default function CommentsContainer() {
                 Query.limit(100),
             ]
         );
-        console.log(response.documents);
-        setMessages(response.documents);
+        // console.log('TOTAL: ', response.documents);
+        const currentPageComments = response.documents.filter(x => x.asset_id == assetId);
+        console.log('PAGE: ', currentPageComments);
+        setMessages(currentPageComments);
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // console.log('MESSAGE:', messageBody);
+
+        const permissions = [
+            Permission.write(Role.user(user.$id)), // inc create, update, delete permissions
+        ];
+
+        let asset = showId ? [showId, 'show'] : (actorId ? [actorId, 'actor'] : [episodeId, 'ep']);
+        const payload = {
+            user_id: user.$id,
+            username: user.name,
+            asset_id: `${asset[1]}--${asset[0]}`,
+            body: messageBody
+        };
+
+        const response = await databases.createDocument(
+            DATABASE_ID,
+            COLLECTION_ID_MESSAGES,
+            ID.unique(),
+            payload,
+            permissions
+        );
+
+        // console.log('RESPONSE:', response);
+
+        // setMessages(prevState => [response, ...prevState]);
+
+        setMessageBody('');
+    }
+
+    const deleteMessage = async (id) => {
+        await databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, id);
+        // for unsubscribed view
+        // setMessages(prevState => prevState.filter(message => message.$id !== id)); 
+    }
 
     return (
         <div className="container">
@@ -90,9 +124,9 @@ export default function CommentsContainer() {
                                     )}
                                     <small className="message-timestamp"> {new Date(message.$createdAt).toLocaleString()}</small>
                                 </p>
+                                {/* same as below for update/edit */}
                                 {message.$permissions.includes(`delete(\"user:${user.$id}\")`) && (
                                     <Trash2 className="delete--btn" onClick={() => { deleteMessage(message.$id) }} />
-
                                 )}
                             </div>
                             <div className={"message--body" + (message.user_id === user.$id ? ' message--body--owner' : '')}>
