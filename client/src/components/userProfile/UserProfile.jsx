a47025d801029ea9184ea6c601357e24a0fb6111
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-07 21:44:41 +0200

    Render stock images dynamically

--- client/src/components/userProfile/UserProfile.jsx
+++ client/src/components/userProfile/UserProfile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './UserProfile.css';
 import { useAuth } from '../../contexts/AuthContext';
 import PageLoader from '../shared/pageLoader/PageLoader';
+import { profileImages } from '../../utilities/profileImages';
 
 export default function UserProfile() {
     const { user } = useAuth();
@@ -53,53 +54,51 @@ export default function UserProfile() {
             <h1>User Profile</h1>
             {loading
                 ? (<PageLoader />)
-                : (
-                    <article className="a-full">
-                        <section>
-                            <div className="inner-section">
-                                <img src={profileImage} alt="profile-img" onClick={openModal} />
-                                <div id="profile-modal" className={`modal${modalOpen ? ' modal-open' : ''}`}>
-                                    <div className="modal-content">
-                                        <div className='tooltip-anchor'>
-                                            <span className="material-symbols-outlined close"
-                                                id="closeModalBtn" onClick={closeModal}>close</span>
-                                            <div className='tooltip'>Close</div>
-                                        </div>
-                                        <h3>Choose a profile image</h3>
-                                        <div className="modal-images">
-                                            <div>
-                                                <img src="/src/assets/tv-1.png" alt="profile-img" data-src="/src/assets/tv-1.png"
-                                                    onClick={chooseProfileImg} />
+                : (<article className="a-full">
+                    <section>
+                        <div className="inner-section">
+                            <img src={profileImage} alt="profile-img" onClick={openModal} />
+                            <div id="profile-modal" className={`modal${modalOpen ? ' modal-open' : ''}`}>
+                                <div className="modal-content">
+                                    <div className='tooltip-anchor'>
+                                        <span className="material-symbols-outlined close"
+                                            id="closeModalBtn" onClick={closeModal}>close</span>
+                                        <div className='tooltip'>Close</div>
+                                    </div>
+                                    <h3>Choose a profile image</h3>
+                                    <div className="modal-images">
+                                        {profileImages.map(x => (
+                                            <div key={x[0] + 'i-m-g'}>
+                                                <img src={`/src/assets/p-img/${x[0]}.${x[1]}`} alt="profile-img"
+                                                    data-src={`/src/assets/p-img/${x[0]}.${x[1]}`} onClick={chooseProfileImg} />
                                             </div>
-
-
-                                        </div>
+                                        ))}
                                     </div>
                                 </div>
                             </div>
-                        </section>
-                        <section>
-                            <div className="inner-section">
-                                <div>
-                                    <p>Username:</p>
-                                    <p><b>{user.name}</b></p>
-                                </div>
-                                <div>
-                                    <p>Email:</p>
-                                    <p><b>{user.email}</b></p>
-                                </div>
-                                <div>
-                                    <p>Created:</p>
-                                    <p><b>{new Date(user.$createdAt).toLocaleString()}</b></p>
-                                </div>
-                                <div>
-                                    <p>Status:</p>
-                                    <p><b>{user.status ? 'Active' : 'Limited'}</b></p>
-                                </div>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="inner-section">
+                            <div>
+                                <p>Username:</p>
+                                <p><b>{user.name}</b></p>
                             </div>
-                        </section>
-                    </article>
-                )}
+                            <div>
+                                <p>Email:</p>
+                                <p><b>{user.email}</b></p>
+                            </div>
+                            <div>
+                                <p>Created:</p>
+                                <p><b>{new Date(user.$createdAt).toLocaleString()}</b></p>
+                            </div>
+                            <div>
+                                <p>Status:</p>
+                                <p><b>{user.status ? 'Active' : 'Limited'}</b></p>
+                            </div>
+                        </div>
+                    </section>
+                </article>)}
         </div>
     )
 }
